3a15a89dc156f93efdb289166fdc061f2f7ad9b7
abdurahimov0101 <abrorjonabdurahimov42@gmail.com>
2024-08-07 15:21:25 +0300

    commit

---
+++ index.js
@@ -0,0 +1,153 @@
+// 1-misol
+
+// let arr = [2, 4, 5, 7, 9];
+// function ToqNumbers(arg) {
+//     let res = [];
+// for (const element of arr) {
+//     if(element % 2 == 1) {
+//         res.push(element)
+//     }
+// }
+// return res
+// }
+
+// console.log(ToqNumbers(arr));
+
+
+// 2-misol
+
+// let str = "sa36lom";
+// function NumbersOdd(arg) {
+//     let coun = 0;
+
+//     for (const element of arg) {
+//         if (element > 0) {
+//             coun++
+//         }
+//     }  
+//     return coun
+// }
+
+
+// console.log(NumbersOdd(str));
+
+// 4-misol
+
+// let arr = [2, 3, 4];
+// function Numbers(arg) {
+// return Math.min(...arg)    
+// }
+// console.log(Numbers(arr));
+
+// 6-misol
+
+// let arr = [2, 4, 4, 5];
+// function KomapytmaOdd(arg) {
+//     let kopaytma = 1;
+// for (const element of arg) {
+//     if (element > 0) { 
+//         kopaytma *= element;
+//       }
+    
+// }
+// return kopaytma
+// }
+
+// console.log(KomapytmaOdd(arr));
+
+// 7-misol
+
+// function Numbersadd(arr) {
+//     return arr.sort((a, b) => b - a);
+//   }
+  
+//   console.log(Numbersadd([3, 1, 4, 1, 5, 9]));
+  
+//   9-misol
+// let str = ["salom "]
+//   function toUpperCaseArray(arr) {
+//     let res = [];
+//     for (let i = 0; i < arr.length; i++) {
+//       res.push(arr[i].toUpperCase());
+//     }
+//     return res;
+//   }
+  
+//   console.log(toUpperCaseArray(str));
+
+// 10-misol
+// function filterOddNumbers(arr) {
+//     let result = [];
+//   for (const element of arr) {
+//     if (element % 2 == 1) {
+//         result.push(element)
+//     }
+//   }
+//     return result;
+//   }
+  
+//   console.log(filterOddNumbers([1, 2, 3, 4, 5]));
+  
+  
+// 12-misol
+// let massiv = [2, 4, 5, 6];
+// function sumArray(arr) {
+//     let sum = 0;
+// for (const element of arr) {
+//     sum += element;
+// }
+//     return sum;
+//   }
+  
+//   console.log(sumArray(massiv));
+
+// 13-misol
+// let massiv = [2, -3, -6, 45, -3]
+// function ManfiySon(arr) {
+//     let result = [];
+//    for (const element of arr) {
+//     if (element >= 0) {
+//         result.push(element);
+//       }
+//    }
+//     return result;
+//   }
+  
+//   console.log(ManfiySon(massiv));
+
+// 15-misol
+// let massiv = [1, 3, 5, 7, 8]
+// function KvadratgaKotarish(arr) {
+//     let result = [];
+//   for (const element of arr) {
+//     result.push(element * element);
+//   }
+//     return result;
+//   }
+  
+//   console.log(KvadratgaKotarish(massiv)); 
+  
+//   17-misol
+
+// let massiv = [1, 3, 4, 6, 8, 9];
+// function TeskarigaOgirish(arr) {
+//     let res = [];
+// for(let i = arr.length - 1; i >= 0; i--) {
+//     res.push(arr[i])
+// }
+// return res
+// }
+// console.log(TeskarigaOgirish(massiv));
+
+// 22-misol
+// let massiv = "salom";
+// function TeskarigaOgirish(arr) {
+//     let res = [];
+// for(let i = arr.length - 1; i >= 0; i--) {
+//     res.push(arr[i])
+// }
+// return res
+// }
+// console.log(TeskarigaOgirish(massiv));
+ 
+  
